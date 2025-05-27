b6c695ea42ee7822bca03e19bda2e850cafceca9
Prashant Tanwar <89059580+prashantt12@users.noreply.github.com>
2025-05-27 10:05:04 +0530

    removed circular initialization

--- src/elastic-search/elastic-search.module.ts
+++ src/elastic-search/elastic-search.module.ts
@@ -2,8 +2,7 @@ import { Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ElasticSearchService } from './elastic-search.service';
 import { ElasticSearchController } from './elastic-search.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ElasticsearchModule, ElasticsearchService } from '@nestjs/elasticsearch';
-import { Client } from '@elastic/elasticsearch';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 @Module({
   providers: [ElasticSearchService],
@@ -23,26 +22,10 @@ import { Client } from '@elastic/elasticsearch';
 
 export class ElasticSearchModule implements OnModuleInit,OnModuleDestroy{
 
-  private readonly client: Client;
-
-  constructor(private readonly elasticsearchService: ElasticsearchService){
-    this.client = new Client({
-      node: process.env.ELASTIC_SEARCH_HOST,
-    })
-  }
-
   async onModuleInit(){
-    await this.client.ping();
-    this.client.on('response', (err, meta) => {
-      if (err) {
-        console.error('Elasticsearch error:', err);
-        return;
-      }
-    });
     console.log('ElasticSearchModule initialized');
   }
   async onModuleDestroy(){
-    await this.client.close()
     console.log('ElasticSearchModule destroyed');
   }
 }
