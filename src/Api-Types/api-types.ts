13dbe0f9ca3287fb80f541642bb32f72de6cde30
Poojith <poojithmeduri@gmail.com>
2025-05-28 12:42:04 +0530

    changed category to event

--- src/Api-Types/api-types.ts
+++ src/Api-Types/api-types.ts
@@ -30,7 +30,7 @@ export const Apis: ApiType = {
         endpoint: 'search-event',
         id: 'bc3f92b8-ab8f-4156-a3e5-97efd229492a', 
     },
-    GET_CATEGORY_DATA: {
+    GET_EVENT_DATA: {
         endpoint: 'get-event-data',
         id: '3e9d0e4e-b9c9-4e16-8468-9910075c9b88',
     }
