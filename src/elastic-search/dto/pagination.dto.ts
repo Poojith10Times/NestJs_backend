10315335ebced3825ecd3ff6d1c46568ad7f2903
Prashant Tanwar <89059580+prashantt12@users.noreply.github.com>
2025-06-05 12:55:14 +0530

    small fix

--- src/elastic-search/dto/pagination.dto.ts
+++ src/elastic-search/dto/pagination.dto.ts
@@ -1,23 +1,32 @@
 import { z } from "zod";
 import { createZodDto } from "nestjs-zod";
 
+// export const PaginationSchema = z.object({
+//     limit: z.string().default("20")
+//     .refine((val) => {
+//             if (Number(val) > 20) {
+//                 return false;
+//             }
+//             return true;
+//         }, {
+//             message: "Limit must be less than or equal to 20"
+//         }),
+//     offset: z.string().default("0")
+//         .transform((val) => Number(val))
+//         .refine((val) => val >= 0, {
+//             message: "Offset must be non-negative",
+//         }),
+// });
+
 export const PaginationSchema = z.object({
-    limit: z.string().default("20")
-    .refine((val) => {
-            if (Number(val) > 20) {
-                return false;
-            }
-            return true;
-        }, {
-            message: "Limit must be less than or equal to 20"
-        }),
-        offset: z
-        .string()
-        .default("0")
-        .transform((val) => Number(val))
-        .refine((val) => val >= 0, {
-            message: "Offset must be non-negative",
-        }),
+    limit: z.preprocess(
+        (val) => Number(val ?? "20"),
+        z.number().max(20, "Limit must be less than or equal to 20")
+    ),
+    offset: z.preprocess(
+        (val) => Number(val ?? "0"),
+        z.number().min(0, "Offset must be non-negative")
+    ),
 });
 
 export class PaginationDto extends createZodDto(PaginationSchema) {}
