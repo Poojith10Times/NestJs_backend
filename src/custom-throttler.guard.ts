627045aac56ef7ff7da89953e6488343d02d214f
Prashant Tanwar <89059580+prashantt12@users.noreply.github.com>
2025-06-11 15:06:51 +0530

    added ip

--- src/custom-throttler.guard.ts
+++ src/custom-throttler.guard.ts
@@ -51,7 +51,7 @@ export class CustomThrottlerGuard extends ThrottlerGuard {
         data: {
           user_id: user?.id || '',
           api_id: api_id || '',
-          ip_address: ip,
+          ip_address: ip || '',
           error_message: error.message,
           endpoint: endpointName || '',
           status_code: HttpStatus.TOO_MANY_REQUESTS,
