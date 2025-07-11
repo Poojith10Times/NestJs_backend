3a02ed0496b6605a185c73d94835a80ed562b678
Prashant Tanwar <89059580+prashantt12@users.noreply.github.com>
2025-07-11 17:08:38 +0530

    print prisma query

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,7 +3,34 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'event',
+          level: 'info',
+        },
+        {
+          emit: 'event',
+          level: 'warn',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+      ],
+    });
+  }
   async onModuleInit() {
     await this.$connect();
+    this.$on('query' as never,(event: { query: string; params: string; duration: number }) => {
+      console.log('Query:', event.query);
+      console.log('Params:', event.params);
+      console.log('Duration:', event.duration, 'ms');
+    });
   }
 }
